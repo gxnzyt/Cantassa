ed9188e2582f141bc08392dadfe9f35c84238454
gxnzyt <aleksbardhi7@gmail.com>
2025-01-11 03:08:28 +0100

    Update script.js

--- clothing-brand/script.js
+++ clothing-brand/script.js
@@ -1,14 +1,11 @@
-// Handle Cookies
 document.addEventListener('DOMContentLoaded', () => {
   const cookiePopup = document.getElementById('cookie-popup');
   const acceptButton = document.getElementById('accept-cookies');
 
-  // Show cookie popup if not previously accepted
   if (!localStorage.getItem('cookiesAccepted')) {
     cookiePopup.classList.add('show');
   }
 
-  // Accept cookies
   acceptButton.addEventListener('click', () => {
     localStorage.setItem('cookiesAccepted', 'true');
     cookiePopup.classList.remove('show');
